e7b699ca744a623198a5c3eb4cefd276b77a43a1
yksz <user4git@gmail.com>
2014-05-09 14:35:50 +0900

    Add cconv.groovy

---
+++ groovy/cconv.groovy
@@ -0,0 +1,99 @@
+import java.nio.charset.Charset
+
+def convertCharset(from, to, src) {
+    def dst = src + ".tmp"
+    convertCharset(from, to, src, dst)
+    moveFile(dst, src)
+}
+
+def convertCharset(from, to, src, dst) {
+    def input  = new FileInputStream(src)
+    def output = new FileOutputStream(dst)
+    def reader = new InputStreamReader(input, from)
+    def writer = new OutputStreamWriter(output, to)
+    def c
+    try {
+        while ((c = reader.read()) != -1)
+            writer.write(c)
+        writer.flush()
+    } finally {
+        reader.close()
+        writer.close()
+    }
+}
+
+def moveFile(src, dst) {
+    def srcFile = new File(src)
+    def dstFile = new File(dst)
+    def ok = srcFile.renameTo(dst)
+    if (!ok) {
+        copyFile(src, dst)
+        srcFile.delete()
+    }
+}
+
+def copyFile(srcFile, dstFile) {
+    def input  = new FileInputStream(srcFile)
+    def output = new FileOutputStream(dstFile)
+    copy(input, output)
+}
+
+def copy(input, output) {
+    def buf = new byte[1024]
+    def len
+    try {
+        while ((len = input.read(buf)) != -1)
+            output.write(buf, 0, len)
+        output.flush();
+    } finally {
+        input.close()
+        output.close()
+    }
+}
+
+def printAvailableCharsets() {
+    def map = Charset.availableCharsets()
+    for (name in map.keySet())
+        println name
+}
+
+
+def cli = new CliBuilder(usage: 'cconv - charset converter')
+cli.f(args:1, 'from encording')
+cli.t(args:1, 'to encording')
+cli.s(args:1, 'src file or src extension(e.g. "*.txt")')
+cli.l('print lists of available charsets')
+cli.h(longOpt:'help', 'print this message')
+def opt = cli.parse(args)
+if (!opt) {
+    System.exit(1)
+}
+if (opt.h) {
+    cli.usage()
+    System.exit(0)
+}
+if (opt.l) {
+    printAvailableCharsets()
+    System.exit(0)
+}
+if (!opt.f || !opt.t || !opt.s) {
+    println 'error: Missing required options: f, t, s'
+    cli.usage()
+    System.exit(1)
+}
+def from = opt.f
+def to = opt.t
+if (opt.s ==~ /^\*\..*$/) {
+    def ext = opt.s
+    new File('.').eachFileRecurse {
+        if (it.isFile() && it.name =~ /.*\.$ext/) {
+            def src = it.getCanonicalPath()
+            println src
+            convertCharset(from, to, src)
+        }
+    }
+} else {
+    def src = opt.s
+    println src
+    convertCharset(from, to, src)
+}
