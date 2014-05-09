87873d24bcdca87db3e707193891aa44a9286715
yksz <user4git@gmail.com>
2014-05-09 23:38:09 +0900

    Add binary check function into cconv.groovy

--- groovy/cconv.groovy
+++ groovy/cconv.groovy
@@ -11,9 +11,8 @@ def convertCharset(from, to, src, dst) {
     def output = new FileOutputStream(dst)
     def reader = new InputStreamReader(input, from)
     def writer = new OutputStreamWriter(output, to)
-    def c
     try {
-        while ((c = reader.read()) != -1)
+        for (def c; (c = reader.read()) != -1;)
             writer.write(c)
         writer.flush()
     } finally {
@@ -24,7 +23,6 @@ def convertCharset(from, to, src, dst) {
 
 def moveFile(src, dst) {
     def srcFile = new File(src)
-    def dstFile = new File(dst)
     def ok = srcFile.renameTo(dst)
     if (!ok) {
         copyFile(src, dst)
@@ -32,22 +30,33 @@ def moveFile(src, dst) {
     }
 }
 
-def copyFile(srcFile, dstFile) {
-    def input  = new FileInputStream(srcFile)
-    def output = new FileOutputStream(dstFile)
+def copyFile(src, dst) {
+    def input  = new FileInputStream(src)
+    def output = new FileOutputStream(dst)
     copy(input, output)
 }
 
 def copy(input, output) {
     def buf = new byte[1024]
-    def len
     try {
-        while ((len = input.read(buf)) != -1)
+        for (def len; (len = input.read(buf)) != -1;)
             output.write(buf, 0, len)
         output.flush();
     } finally {
         input.close()
         output.close()
+    }
+}
+
+def isBinaryFile(path) {
+    def input = new FileInputStream(path)
+    try {
+        for (def b; (b = input.read()) != -1;)
+            if (b == 0)
+                return true
+        return false
+    } finally {
+        input.close()
     }
 }
 
@@ -61,7 +70,7 @@ def printAvailableCharsets() {
 def cli = new CliBuilder(usage: 'cconv - charset converter')
 cli.f(args:1, 'from encording')
 cli.t(args:1, 'to encording')
-cli.s(args:1, 'search for files by filepath or regexp(e.g. "/.*\\.txt/")')
+cli.s(args:1, 'search for files by path or regexp(e.g. "/.*\\.txt/")')
 cli.l('print lists of available charsets')
 cli.h(longOpt:'help', 'print this message')
 def opt = cli.parse(args)
@@ -86,7 +95,7 @@ def to = opt.t
 if (opt.s ==~ '^/.*/$') {
     def regexp = opt.s.substring(1, opt.s.length() - 1)
     new File('.').eachFileRecurse {
-        if (it.isFile() && it.name =~ regexp) {
+        if (it.isFile() && !isBinaryFile(it) && it.name =~ regexp) {
             def src = it.getCanonicalPath()
             println src
             convertCharset(from, to, src)
