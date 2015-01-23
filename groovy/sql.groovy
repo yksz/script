5e5c324c49f5271a9365d0ec123717b82a203aab
yksz <user4git@gmail.com>
2015-01-23 13:42:00 +0900

    Add sql.groovy

---
+++ groovy/sql.groovy
@@ -0,0 +1,66 @@
+#!/usr/bin/env groovy
+/**
+ * SQL Scripts Runner
+ */
+
+@Grapes([
+    @Grab(group='com.h2database', module='h2', version='1.4.185'),
+    @Grab(group='org.apache.derby', module='derby', version='10.11.1.1')
+])
+import groovy.sql.Sql
+
+def newSql(dbPropPath) {
+    def db = new Properties()
+    if (dbPropPath) {
+        new File(dbPropPath).withInputStream { db.load(it) }
+    } else {
+        db.driver = 'org.h2.Driver'
+        db.url = 'jdbc:h2:mem:'
+        db.user = 'sa'
+        db.password = ''
+    }
+    return Sql.newInstance(db.url, db.user, db.password, db.driver)
+}
+
+def runSqlScript(sql, scriptPath) {
+    def script = new File(scriptPath).text
+    script = script.replaceAll(/--.*/, '') // remove comment
+    script = removeBlankLine(script)
+    script.split(/;\s*/).each {
+        if (!it.matches(/\s+/)) {
+            println '# execute sql:'
+            println it
+            sql.execute(it)
+            println ''
+        }
+    }
+}
+
+def removeBlankLine(text) {
+    def sb = new StringBuilder()
+    text.eachLine {
+        if (it != '')
+            sb.append("$it\n")
+    }
+    return sb.toString()
+}
+
+
+def cli = new CliBuilder(usage: './sql.groovy [options] <files>')
+cli.with {
+    _ longOpt:'db', args:1, argName:'file', 'a properties file of database'
+    h longOpt:'help', 'print this message'
+}
+def opt = cli.parse(args)
+if (opt.h) {
+    cli.usage()
+    System.exit(0)
+}
+if (opt.arguments().size() < 1) {
+    cli.usage()
+    System.exit(1)
+}
+def sql = newSql(opt.db)
+opt.arguments().each { arg ->
+    runSqlScript(sql, arg)
+}
