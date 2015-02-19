955c54d0fff211cb3119697e408610aec5e1a678
yksz <user4git@gmail.com>
2015-02-19 17:52:01 +0900

    Enable sql.groovy to execute command line statments

--- groovy/sql.groovy
+++ groovy/sql.groovy
@@ -20,12 +20,12 @@ def newSql(dbPropPath) {
         db.user = 'sa'
         db.password = ''
     }
-    println "connect to ${db.url}"
+    println "# connect to ${db.url}"
     return Sql.newInstance(db.url, db.user, db.password, db.driver)
 }
 
-def runSqlScript(sql, scriptPath) {
-    executeSql(sql, parseSqlScript(scriptPath))
+def runSqlScript(sql, scriptPath, cnt) {
+    executeSql(sql, parseSqlScript(scriptPath), cnt)
 }
 
 def parseSqlScript(scriptPath) {
@@ -49,20 +49,25 @@ def removeBlankLine(text) {
     return sb.toString()
 }
 
-def executeSql(sql, statements) {
-    println '# execute sql with batch:'
-    sql.withBatch(statements.size()) { stmt ->
-        statements.each {
-            println "$it\n"
-            stmt.addBatch(it)
+def executeSql(sql, statements, cnt) {
+    println '# execute sql with batch'
+    for (i in 0..<cnt) {
+        if (i == 1) println "# ...and repeat ${cnt - 1} times"
+        sql.withBatch(statements.size()) { stmt ->
+            statements.each {
+                if (i == 0) println "$it\n"
+                stmt.addBatch(it)
+            }
         }
     }
 }
 
 
-def cli = new CliBuilder(usage: './sql.groovy [options] <files>')
+def cli = new CliBuilder(usage: './sql.groovy [options] <files> | <sql statements>')
 cli.with {
     _ longOpt:'db', args:1, argName:'file', 'a properties file of database'
+    c args:1, argName:'count', 'a repeat count'
+    s 'execute sql statements'
     h longOpt:'help', 'print this message'
 }
 def opt = cli.parse(args)
@@ -74,7 +79,15 @@ if (opt.arguments().size() < 1) {
     cli.usage()
     System.exit(1)
 }
+def cnt = (opt.c ?: 1) as int
 def sql = newSql(opt.db)
-opt.arguments().each { arg ->
-    runSqlScript(sql, arg)
+def start = System.currentTimeMillis()
+if (opt.s) {
+    executeSql(sql, opt.arguments(), cnt)
+} else {
+    opt.arguments().each { arg ->
+        runSqlScript(sql, arg, cnt)
+    }
 }
+def stop = System.currentTimeMillis()
+println "# ${(stop - start) / 1000} [sec]"
