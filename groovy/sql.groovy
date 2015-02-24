2170742cb9a878f316334673856c18cd62983259
yksz <user4git@gmail.com>
2015-02-24 18:38:04 +0900

    Enable select statement and add options in sql.groovy

--- groovy/sql.groovy
+++ groovy/sql.groovy
@@ -6,20 +6,12 @@
 @GrabConfig(systemClassLoader=true)
 @Grapes([
     @Grab(group='com.h2database', module='h2', version='1.4.185'),
-    @Grab(group='org.apache.derby', module='derby', version='10.11.1.1')
+    @Grab(group='org.apache.derby', module='derby', version='10.11.1.1'),
+    @Grab(group='org.apache.derby', module='derbyclient', version='10.11.1.1')
 ])
 import groovy.sql.Sql
 
-def newSql(dbPropPath) {
-    def db = new Properties()
-    if (dbPropPath) {
-        new File(dbPropPath).withInputStream { db.load(it) }
-    } else {
-        db.driver = 'org.h2.Driver'
-        db.url = 'jdbc:h2:mem:'
-        db.user = 'sa'
-        db.password = ''
-    }
+def newSql(db) {
     println "# connect to ${db.url}"
     return Sql.newInstance(db.url, db.user, db.password, db.driver)
 }
@@ -50,13 +42,16 @@ def removeBlankLine(text) {
 }
 
 def executeSql(sql, statements, cnt) {
-    println '# execute sql with batch'
     for (i in 0..<cnt) {
         if (i == 1) println "# ...and repeat ${cnt - 1} times"
-        sql.withBatch(statements.size()) { stmt ->
-            statements.each {
-                if (i == 0) println "$it\n"
-                stmt.addBatch(it)
+        statements.each { stmt ->
+            if (i == 0) println "# execute sql: $stmt"
+            if (stmt.trim().toLowerCase().startsWith('select ')) {
+                sql.eachRow(stmt) { row ->
+                    println row
+                }
+            } else {
+                sql.execute(stmt)
             }
         }
     }
@@ -65,7 +60,11 @@ def executeSql(sql, statements, cnt) {
 
 def cli = new CliBuilder(usage: './sql.groovy [options] <files> | <sql statements>')
 cli.with {
-    _ longOpt:'db', args:1, argName:'file', 'a properties file of database'
+    _ longOpt:'prop', args:1, argName:'file', 'jdbc properties file'
+    _ longOpt:'driver', args:1, 'jdbc driver'
+    _ longOpt:'url', args:1, 'jdbc url'
+    _ longOpt:'user', args:1, 'jdbc user'
+    _ longOpt:'password', args:1, 'jdbc password'
     c args:1, argName:'count', 'a repeat count'
     s 'execute sql statements'
     h longOpt:'help', 'print this message'
@@ -80,7 +79,7 @@ if (opt.arguments().size() < 1) {
     System.exit(1)
 }
 def cnt = (opt.c ?: 1) as int
-def sql = newSql(opt.db)
+def sql = newSql(loadDbConfig(opt))
 def start = System.currentTimeMillis()
 if (opt.s) {
     executeSql(sql, opt.arguments(), cnt)
@@ -91,3 +90,22 @@ if (opt.s) {
 }
 def stop = System.currentTimeMillis()
 println "# ${(stop - start) / 1000} [sec]"
+
+def loadDbConfig(opt) {
+    def db = new Properties()
+    // default
+    db.driver = 'org.h2.Driver'
+    db.url = 'jdbc:h2:mem:'
+    db.user = 'sa'
+    db.password = ''
+    // file
+    if (opt.prop) {
+        new File(opt.prop).withInputStream { db.load(it) }
+    }
+    // command option
+    db.driver = opt.driver ?: db.driver
+    db.url = opt.url ?: db.url
+    db.user = opt.user ?: db.user
+    db.password = opt.password ?: db.password
+    return db
+}
