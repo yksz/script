c8282a79bf8750cd5c5f7f6fb63db12adb9f54dd
yksz <user4git@gmail.com>
2015-04-03 17:25:46 +0900

    Refactor replace.sh

--- shell/replace.sh
+++ shell/replace.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-##
-## Replace a string in multiple files
-##
+#
+# Replace a string in multiple files
+#
 
-if [ $# -lt 3 ]; then
-    echo 'usage: ./replace.sh <old string> <new string> <file>'
-    echo '  e.g. ./replace.sh lang long "*.txt"'
+if [ $# -lt 3 ] ; then
+    echo "usage: $0 <old string> <new string> <file>"
+    echo "  e.g. $0 lang long \"*.txt\""
     exit 1
 fi
 
