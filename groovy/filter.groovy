b1054fe55f382af9657c944c9b46158047da1089
yksz <user4git@gmail.com>
2015-09-03 13:57:55 +0900

    Modify filter.groovy to support multi words

--- groovy/filter.groovy
+++ groovy/filter.groovy
@@ -7,7 +7,8 @@ import java.util.regex.Pattern
 
 def cli = new CliBuilder(usage: './filter.groovy <word> <file>')
 cli.with {
-    n longOpt:'not', 'select non-matching lines'
+    d args:1, argName:'delimiter', 'default delimiter is ","'
+    n 'select non-matching lines'
     h longOpt:'help', 'print this message'
 }
 def opt = cli.parse(args)
@@ -20,9 +21,14 @@ if (opt.arguments().size() < 2) {
     System.exit(1)
 }
 
+def delimiter = opt.d ?: ','
+def match = !opt.n
+def words = opt.arguments()[0].split(delimiter)
 def file = new File(opt.arguments()[1])
-def word = opt.arguments()[0]
-def matched = !opt.n
 System.out << file.filterLine { line ->
-    line.contains(word) == matched
+    def contained = false
+    for (word in words) {
+        contained |= line.contains(word)
+    }
+    return contained == match
 }
