f821aba2b2670a6c9bc2a7230fc98775465bacaa
yksz <user4git@gmail.com>
2015-09-27 23:15:42 +0900

    Add tcps.groovy

---
+++ groovy/tcps.groovy
@@ -0,0 +1,76 @@
+#!/usr/bin/env groovy
+/**
+ * TCP Server
+ */
+
+import java.util.concurrent.atomic.AtomicInteger
+
+abstract class Server {
+    def port = 8080
+
+    def run() {
+        def serverSocket = new ServerSocket(port)
+        println "this server is listening on port $port"
+        def id = new AtomicInteger(0)
+        while (true) {
+            serverSocket.accept { socket ->
+                processConnection(id.getAndIncrement(), socket)
+            }
+        }
+    }
+
+    def processConnection(id, socket) {
+        println "id=$id: connected [ip=${socket.getInetAddress()}]"
+        def input = new BufferedInputStream(socket.getInputStream())
+        def output = new BufferedOutputStream(socket.getOutputStream())
+        try {
+            while (serve(id, input, output)) {}
+        } catch (IOException e) {
+            println "id=$id: $e"
+        }
+        input.close()
+        output.close()
+        println "id=$id: disconnected"
+    }
+
+    def serve(input, output) {}
+}
+
+class TCPServer extends Server {
+    def serve(id, input, output) {
+        def line = readLine(input)
+        if (line == null) {
+            return false
+        }
+        println line
+        return true
+    }
+
+    def readLine(input) {
+        def buf = new StringBuilder()
+        while (true) {
+            int c = input.read()
+            if (c == -1)
+                return buf.length() > 0 ? buf.toString() : null
+            else if (c == "\n")
+                return buf.toString()
+            else
+                buf.append((char) c)
+        }
+    }
+}
+
+
+def cli = new CliBuilder(usage: './echos.groovy [options]')
+cli.with {
+    p args:1, 'port'
+    h longOpt:'help', 'print this message'
+}
+def opt = cli.parse(args)
+if (opt.h) {
+    cli.usage()
+    System.exit(0)
+}
+def port = (opt.p ?: 8080) as int
+def server = new TCPServer(port: port)
+server.run()
