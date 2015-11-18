ccb3666083ead2cf765d5bd96bddf7700d643694
yksz <user4git@gmail.com>
2015-11-18 13:55:06 +0900

    Enable tcpc.groovy to send a binary message

--- groovy/tcpc.groovy
+++ groovy/tcpc.groovy
@@ -3,8 +3,27 @@
  * TCP Client
  */
 
-def send(String message) {
+import java.nio.ByteBuffer
+
+def sendText(String message) {
+    if (message.endsWith('\\n')) {
+        message = replaceLast(message, '\\n', '\n')
+    }
     def data = message.getBytes()
+    socket.outputStream.write(data);
+}
+
+def replaceLast(String str, String target, String replacement) {
+    def sb = new StringBuilder(str)
+    def lastIndex = str.lastIndexOf(target)
+    if (lastIndex == -1)
+        return str
+    sb.replace(lastIndex, lastIndex + target.size(), replacement)
+    return sb.toString()
+}
+
+def sendBinary(String message) {
+    def data = Integer.decode(message) as byte
     socket.outputStream.write(data);
 }
 
@@ -24,16 +43,24 @@ def repl() {
 def eval(String line) {
     if (line == null || line.trim().isEmpty())
         return
-    send(line)
+    isBinaryMode ? sendBinary(line) : sendText(line)
 }
 
 
-if (args.length < 2) {
-    println 'usage: ./tcpc.groovy <host> <port>'
-    System.exit(1)
+def cli = new CliBuilder(usage: './tcpc.groovy [options] <host> <port>')
+cli.with {
+    b 'binary mode'
+    h longOpt:'help', 'print this message'
 }
-def host = args[0]
-def port = args[1] as int
+def opt = cli.parse(args)
+if (opt.h) {
+    cli.usage()
+    System.exit(0)
+}
+
+isBinaryMode = opt.b
+def host = opt.arguments()[0]
+def port = opt.arguments()[1] as int
 socket = new Socket(host, port)
 repl()
 socket.close()
