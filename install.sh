65bba471bbc231a46b51437d519122cc526d1f94
yksz <user4git@gmail.com>
2016-03-04 19:26:05 +0900

    Enable install.sh to run on Ubuntu

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ $# -lt 1 ] ; then
     echo "usage: $0 <install path>"
     exit 1
 fi
 
-TARGET_DIR=('shell')
+TARGET_DIRS=('shell')
 
 install_path=$1
-echo install to $install_path
+mkdir -p ${install_path}
+echo Install to $install_path
 
 script_dir=$(cd $(dirname $0); pwd)
-for dir in ${TARGET_DIR[@]} ; do
+for dir in ${TARGET_DIRS[@]} ; do
     dir_path="$script_dir/$dir"
     script_names=(`ls $dir_path`)
     for script_name in ${script_names[@]} ; do
